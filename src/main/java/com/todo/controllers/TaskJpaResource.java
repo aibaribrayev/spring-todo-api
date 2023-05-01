bff65aa81a92d5bc328dfd4ebb1f4d3cbf862c08
AibarIbray <aibar.s.ibrayev@gmail.com>
2023-05-01 23:21:36 +0600

    put requests for tasks

--- src/main/java/com/todo/controllers/TaskJpaResource.java
+++ src/main/java/com/todo/controllers/TaskJpaResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,7 +28,7 @@ public class TaskJpaResource {
     }
 
     @PutMapping("/jpa/task/{id}")
-    public ResponseEntity<Object> updateTaskStatus(@PathVariable int id, @RequestBody Task updatedTask) {
+    public ResponseEntity<Object> updateTask(@PathVariable int id, @RequestBody Task updatedTask) {
         Optional<Task> optionalTask = taskRepository.findById(id);
 
         if(optionalTask.isEmpty())
@@ -36,6 +37,43 @@ public class TaskJpaResource {
         savedTask.setDescription(updatedTask.getDescription());
         savedTask.setDueDate(updatedTask.getDueDate());
         savedTask.setCompleted(updatedTask.getCompleted());
+
+        Task updated = taskRepository.save(savedTask); // save the updated Task object to the database
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(updated.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/jpa/task/status/{id}")
+    public ResponseEntity<Object> updateTaskStatus(@PathVariable int id, @RequestBody boolean isCompleted) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+
+        if(optionalTask.isEmpty())
+            throw new TaskNotFoundException("id:"+id);
+        Task savedTask = optionalTask.get();
+        savedTask.setCompleted(isCompleted);
+
+        Task updated = taskRepository.save(savedTask); // save the updated Task object to the database
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(updated.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+    @PutMapping("/jpa/task/duedate/{id}")
+    public ResponseEntity<Object> updateTaskDueDate(@PathVariable int id, @RequestBody LocalDateTime newDate) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+
+        if(optionalTask.isEmpty())
+            throw new TaskNotFoundException("id:"+id);
+        Task savedTask = optionalTask.get();
+        savedTask.setDueDate(newDate);
 
         Task updated = taskRepository.save(savedTask); // save the updated Task object to the database
 
